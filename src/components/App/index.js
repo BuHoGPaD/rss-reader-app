8b2320e6029b2cbf4f26e96becf032cf58138dd8
vskalozub <vasily.skalozub@volia-software.com>
2018-03-17 21:44:58 +0200

    feat: Added simple life-cicle process.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {
+  changeText,
+} from '../../actions';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component { 
+class Login extends Component { 
+  constructor(props) {
+    super(props);
+    this.changeText = this.changeText.bind(this);
+    this.submit = this.submit.bind(this);
+
+    this.state = {
+        value: ''
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(nextProps.counter);
+  }
+
+  changeText(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  submit() {
+    this.props.initSubmit(this.state);
+  }
 
   render() {
     return (
       <div className="container">
-        App
+        <form>
+          <div className="form-group">
+            <label htmlFor="inputEmailField">Email</label>
+            <input id="inputEmailField" type="text" className="form-control" value={this.state.value} onChange={this.changeText}>
+            </input>
+          </div>
+
+          <button type="button" className="btn btn-success" style={{ marginBottom: '1px' }} onClick={this.submit}>Submit</button>
+          
+        </form>
       </div>
     );
   }
@@ -18,8 +52,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    
+    initSubmit: (data) => {
+      dispatch(changeText(data));
+    }
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
